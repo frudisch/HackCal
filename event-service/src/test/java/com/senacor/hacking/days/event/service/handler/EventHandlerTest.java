1ea9e8f35428952d9441a6345a047993df69e491
Rudisch, Florian <Florian.Rudisch@senacor.com>
2018-11-22 11:03:47 +0100

    Added additional EventHandler Tests

--- event-service/src/test/java/com/senacor/hacking/days/event/service/handler/EventHandlerTest.java
+++ event-service/src/test/java/com/senacor/hacking/days/event/service/handler/EventHandlerTest.java
@@ -11,6 +11,7 @@ import io.helidon.webserver.json.JsonSupport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -19,6 +20,7 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -37,7 +39,7 @@ public class EventHandlerTest {
     @InjectMocks
     private EventHandler uut;
 
-    @Mock
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private ServerRequest serverRequest;
 
     @Mock
@@ -76,7 +78,103 @@ public class EventHandlerTest {
 
         uut.createEvent(serverRequest, serverResponse, testJson);
 
-        verify(serverResponse, times(1)).send(eq(testEvent));
+        verify(serverResponse, times(1)).send(eq(Json.createObjectBuilder()
+                .add("name", "test-name")
+                .add("date", LocalDateTime.of(2018, 11, 21, 12, 12).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .add("description", "test-description")
+                .add("uuid", "7ad93e43-3537-473b-8ff8-99b16895c66d")
+                .build()));
+    }
+
+    @Test
+    @DisplayName("Test a successful event update request")
+    public void testSuccessfulEventUpdate() {
+        JsonObject testJson = Json.createObjectBuilder()
+                .add("name", "test-update")
+                .add("date", LocalDateTime.of(2018, 11, 21, 12, 12).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .add("description", "test-description")
+                .build();
+
+        Event testEvent = Event.builder()
+                .name("test-update")
+                .uuid(UUID.fromString("7ad93e43-3537-473b-8ff8-99b16895c66d"))
+                .description("test-description")
+                .date(LocalDateTime.of(2018, 11, 21, 12, 12))
+                .build();
+
+        when(eventService.updateEvent(eq(UUID.fromString("7ad93e43-3537-473b-8ff8-99b16895c66d")), eq(Event.builder()
+                .name("test-update")
+                .description("test-description")
+                .date(LocalDateTime.of(2018, 11, 21, 12, 12))
+                .build()))).thenReturn(testEvent);
+
+        when(serverRequest.path().param(eq("id"))).thenReturn("7ad93e43-3537-473b-8ff8-99b16895c66d");
+
+        uut.updateEvent(serverRequest, serverResponse, testJson);
+
+        verify(serverResponse, times(1)).send(eq(Json.createObjectBuilder()
+                .add("name", "test-update")
+                .add("date", LocalDateTime.of(2018, 11, 21, 12, 12).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .add("description", "test-description")
+                .add("uuid", "7ad93e43-3537-473b-8ff8-99b16895c66d")
+                .build()));
+    }
+
+    @Test
+    @DisplayName("Test a successful event delete request")
+    public void testSuccessfulEventDelete() {
+        when(serverRequest.path().param(eq("id"))).thenReturn("7ad93e43-3537-473b-8ff8-99b16895c66d");
+
+        uut.deleteEvent(serverRequest, serverResponse);
+
+        verify(eventService, times(1)).deleteEvent(eq(UUID.fromString("7ad93e43-3537-473b-8ff8-99b16895c66d")));
+        verify(serverResponse, times(1)).send();
+    }
+
+    @Test
+    @DisplayName("Test a successful event selection request by the event-id")
+    public void testSuccessfulEventSelection() {
+        Event testEvent = Event.builder()
+                .name("test-name")
+                .uuid(UUID.fromString("7ad93e43-3537-473b-8ff8-99b16895c66d"))
+                .description("test-description")
+                .date(LocalDateTime.of(2018, 11, 21, 12, 12))
+                .build();
+
+        when(serverRequest.path().param(eq("id"))).thenReturn("7ad93e43-3537-473b-8ff8-99b16895c66d");
+
+        when(eventService.getEventById(eq(UUID.fromString("7ad93e43-3537-473b-8ff8-99b16895c66d")))).thenReturn(testEvent);
+
+        uut.getEventById(serverRequest, serverResponse);
+
+        verify(serverResponse, times(1)).send(eq(Json.createObjectBuilder()
+                .add("name", "test-name")
+                .add("date", LocalDateTime.of(2018, 11, 21, 12, 12).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .add("description", "test-description")
+                .add("uuid", "7ad93e43-3537-473b-8ff8-99b16895c66d")
+                .build()));
+    }
+
+    @Test
+    @DisplayName("Test a successful complete event list selection request")
+    public void testSuccessfulAllEventSelection() {
+        Event testEvent = Event.builder()
+                .name("test-name")
+                .uuid(UUID.fromString("7ad93e43-3537-473b-8ff8-99b16895c66d"))
+                .description("test-description")
+                .date(LocalDateTime.of(2018, 11, 21, 12, 12))
+                .build();
+
+        when(eventService.getAllEvents()).thenReturn(Collections.singletonList(testEvent));
+
+        uut.getAllEvents(serverRequest, serverResponse);
+
+        verify(serverResponse, times(1)).send(eq(Collections.singletonList(Json.createObjectBuilder()
+                .add("name", "test-name")
+                .add("date", LocalDateTime.of(2018, 11, 21, 12, 12).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                .add("description", "test-description")
+                .add("uuid", "7ad93e43-3537-473b-8ff8-99b16895c66d")
+                .build())));
     }
 
     @Test
